8ef68845e9d4879ae1a354f229d148e1c9ca680a
kirizat-theosophe <54266892+kirizat-theosophe@users.noreply.github.com>
2021-09-14 14:55:45 +0200

    Initial Commit

---
+++ Calculeter_2Oppgave/Calculeter_2OppgaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculeter_2OppgaveApp.swift
+//  Calculeter_2Oppgave
+//
+//  Created by Bruker on 14/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct Calculeter_2OppgaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculeter_2Oppgave/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculeter_2Oppgave
+//
+//  Created by Bruker on 14/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
